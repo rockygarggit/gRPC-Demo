3800937626ae1d70fe2883d213e14530d7f5ff88
Rocky Garg <rockyg@bdfgroup.com>
2020-07-21 11:52:11 -0500

    init

---
+++ gRPCClient/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Grpc.Net.Client;
+using gRPCDemo.Protos;
+using gRPCDemo;
+using System.Threading.Tasks;
+using System.Linq;
+using Grpc.Core;
+
+namespace gRPCClient
+{
+    class Program
+    {
+        static  async Task Main(string[] args)
+        {
+
+            Console.WriteLine("============= Unary mode Example=====================");
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var productsClient = new ProductGrpcService.ProductGrpcServiceClient(channel);
+            var productResponse = await productsClient.GetProductsAsync(new ProductRequest());
+            var products = productResponse.Products.ToList<Products>();
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Product Id: {product.ProductId},Name :  {product.Name},  Description : {product.ProductDescription},  Price: {product.ProductPrice}");
+            }
+
+            Console.WriteLine("\n\r\n\r");
+            Console.WriteLine("============= Server Streaming Example=====================");
+
+            using (var call = productsClient.GetNewProducts(new ProductRequest()))
+            {
+                while (await call.ResponseStream.MoveNext())
+                {
+                    var product = call.ResponseStream.Current;
+
+                    Console.WriteLine($"Product Id: {product.ProductId},Name :  {product.Name},  Description : {product.ProductDescription},  Price: {product.ProductPrice}");
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ gRPCDemo/Services/ProductService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using gRPCDemo.Protos;
+using Microsoft.Extensions.Logging;
+
+namespace gRPCDemo.Services
+{
+    public class ProductService : gRPCDemo.Protos.ProductGrpcService.ProductGrpcServiceBase
+    {
+        private readonly ILogger<ProductService> _logger;
+        public ProductService(ILogger<ProductService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<ProductResponse> GetProducts(ProductRequest request, ServerCallContext context)
+        {
+            ProductResponse response = new ProductResponse();
+            response.Products.Add(new Products { Name = "Awesome Chair", ProductDescription = "Simple Chair", ProductId = 1, ProductPrice = 100.00F });
+            response.Products.Add(new Products { Name = "Awesome Table", ProductDescription = "Simple Table", ProductId = 2, ProductPrice = 300.00F });
+            response.Products.Add(new Products { Name = "Awesome Sofa", ProductDescription = "Simple Sofa", ProductId = 3, ProductPrice = 400.00F });
+
+            return Task.FromResult(response);
+        }
+
+
+        public override async Task GetNewProducts(ProductRequest request, IServerStreamWriter<Products> responseStream, ServerCallContext context)
+        {
+            List<Products> newProducts = new List<Products>()
+            {
+                new Products
+                {
+                    Name = "New Chair",
+                    ProductDescription = "New Awesome Chair",
+                    ProductId = 1001,
+                    ProductPrice = 50.00F
+                },
+                new Products
+                {
+                    Name = "New Chair",
+                    ProductDescription = "New Awesome Chair",
+                    ProductId = 1001,
+                    ProductPrice = 50.00F
+                },
+                new Products
+                {
+                    Name = "New Chair",
+                    ProductDescription = "New Awesome Chair",
+                    ProductId = 1001,
+                    ProductPrice = 50.00F
+                }
+            };
+
+
+            foreach (var product in newProducts)
+            {
+                await Task.Delay(1000);
+                await responseStream.WriteAsync(product);
+            }
+
+        }
+    }
+}
